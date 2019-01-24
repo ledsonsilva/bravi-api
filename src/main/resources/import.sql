d35630c7136ea9135c478740272acab471013a55
LEDSON Oliveira da Silva <05168332196@poupex.com.br>
2019-01-24 08:07:58 -0200

    Update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,14 +1,15 @@
 INSERT INTO BRAVI.PERSON (id, name) VALUES (1, 'Ledson Silva');
 INSERT INTO BRAVI.CONTACT(id, contact, type, person_id) VALUES (1, '61991740585', 'PHONE', 1);
+INSERT INTO BRAVI.CONTACT(id, contact, type, person_id) VALUES (2, 'ledson.dev@gmail.com', 'EMAIL', 1);
 
 INSERT INTO BRAVI.PERSON (id, name) VALUES (2, 'Israel Luís Ferreira da Silva');
-INSERT INTO BRAVI.CONTACT(id, contact, type, person_id) VALUES (2, '61991740585', 'WHATS', 2);
+INSERT INTO BRAVI.CONTACT(id, contact, type, person_id) VALUES (3, '61991740585', 'WHATS', 2);
 
 INSERT INTO BRAVI.PERSON (id, name) VALUES (3, 'Rayanne Silva Ferreira');
-INSERT INTO BRAVI.CONTACT(id, contact, type, person_id) VALUES (3, 'rsilva@gmail.com', 'EMAIL', 3);
+INSERT INTO BRAVI.CONTACT(id, contact, type, person_id) VALUES (4, 'rsilva@gmail.com', 'EMAIL', 3);
 
 INSERT INTO BRAVI.PERSON (id, name) VALUES (4, 'João Pereira');
-INSERT INTO BRAVI.CONTACT(id, contact, type, person_id) VALUES (4, '61991740585', 'PHONE', 4);
+INSERT INTO BRAVI.CONTACT(id, contact, type, person_id) VALUES (5, '61991740585', 'PHONE', 4);
 
 INSERT INTO BRAVI.PERSON (id, name) VALUES (5, 'Apenas uma massa de dados');
-INSERT INTO BRAVI.CONTACT(id, contact, type, person_id) VALUES (5, 'massadados@globo.com', 'email', 5);
+INSERT INTO BRAVI.CONTACT(id, contact, type, person_id) VALUES (6, 'massadados@globo.com', 'EMAIL', 5);
